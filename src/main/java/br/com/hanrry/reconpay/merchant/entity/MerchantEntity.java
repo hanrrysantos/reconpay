3721a1f56669af740cd798bc836349607b6d19aa
hanrrysantos <hanrry.jsantos@gmailcom>
2026-05-29 13:37:19 -0300

    feat: adicionando merchant entity

---
+++ src/main/java/br/com/hanrry/reconpay/merchant/entity/MerchantEntity.java
@@ -0,0 +1,50 @@
+package br.com.hanrry.reconpay.merchant.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Setter
+@Getter
+@Entity
+@Table(name = "tb_merchants")
+public class MerchantEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 30)
+    private String document;
+
+    @Column(nullable = false)
+    private boolean active;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate(){
+        LocalDateTime now = LocalDateTime.now();
+
+        this.createdAt = now;
+        this.updatedAt = now;
+        this.active = true;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
