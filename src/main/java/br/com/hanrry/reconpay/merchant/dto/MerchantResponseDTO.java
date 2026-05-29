f7871c1fadf57b0099aa8952c052dedfed952d79
hanrrysantos <hanrry.jsantos@gmailcom>
2026-05-29 13:52:36 -0300

    fix: renomeando os dtos

--- src/main/java/br/com/hanrry/reconpay/merchant/dto/MerchantResponseDTO.java
+++ src/main/java/br/com/hanrry/reconpay/merchant/dto/MerchantResponseDTO.java
@@ -3,7 +3,7 @@ package br.com.hanrry.reconpay.merchant.dto;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record CreateMerchantResponseDTO (
+public record MerchantResponseDTO(
         UUID id,
         String name,
         String document,
