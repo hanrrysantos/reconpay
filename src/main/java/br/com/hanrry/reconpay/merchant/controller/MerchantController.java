37bbc953420068e4392b7b051fc0e8605f470e3f
hanrrysantos <hanrry.jsantos@gmail.com>
2026-06-15 20:31:53 -0300

    feat: implementando endpoints para receber as requisicoes

--- src/main/java/br/com/hanrry/reconpay/merchant/controller/MerchantController.java
+++ src/main/java/br/com/hanrry/reconpay/merchant/controller/MerchantController.java
@@ -1,9 +1,67 @@
 package br.com.hanrry.reconpay.merchant.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.hanrry.reconpay.merchant.dto.MerchantRequestDTO;
+import br.com.hanrry.reconpay.merchant.dto.MerchantResponseDTO;
+import br.com.hanrry.reconpay.merchant.dto.UpdateMerchantRequestDTO;
+import br.com.hanrry.reconpay.merchant.service.MerchantService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
-@RequestMapping(value = "/merchants")
+@RequestMapping(value = "/api/merchants")
+@RequiredArgsConstructor
 public class MerchantController {
+
+    private final MerchantService merchantService;
+
+    @PostMapping()
+    public ResponseEntity<MerchantResponseDTO> createMerchant(
+            @Valid
+            @RequestBody MerchantRequestDTO request){
+        MerchantResponseDTO merchant = merchantService.createMerchant(request);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(merchant.id()).toUri();
+
+        return ResponseEntity.created(uri).body(merchant);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<MerchantResponseDTO>> findAllMerchants() {
+        List<MerchantResponseDTO> merchant = merchantService.findAllMerchants();
+
+        return ResponseEntity.ok().body(merchant);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<MerchantResponseDTO> findMerchantById(
+            @PathVariable UUID id){
+        MerchantResponseDTO merchant = merchantService.findMerchantById(id);
+
+        return ResponseEntity.ok().body(merchant);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<MerchantResponseDTO> updateMerchantById(
+            @PathVariable UUID id,
+            @RequestBody UpdateMerchantRequestDTO request){
+        MerchantResponseDTO merchant = merchantService.updateMerchantById(id, request);
+
+        return ResponseEntity.ok().body(merchant);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteMerchantById(@PathVariable UUID id){
+        merchantService.deleteMerchantById(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+
 }
