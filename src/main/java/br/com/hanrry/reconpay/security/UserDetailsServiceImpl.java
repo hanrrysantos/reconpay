1b69962d6c9651b83a4ff810cf49319cee71ec2e
hanrrysantos <hanrry.jsantos@gmail.com>
2026-06-15 20:42:22 -0300

    fix: mudando nome da dependencia

--- src/main/java/br/com/hanrry/reconpay/security/UserDetailsServiceImpl.java
+++ src/main/java/br/com/hanrry/reconpay/security/UserDetailsServiceImpl.java
@@ -1,7 +1,7 @@
 package br.com.hanrry.reconpay.security;
 
 import br.com.hanrry.reconpay.auth.entity.UserEntity;
-import br.com.hanrry.reconpay.auth.repository.UserRepository;
+import br.com.hanrry.reconpay.auth.repository.IUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,11 +12,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    private final UserRepository userRepository;
+    private final IUserRepository IUserRepository;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        UserEntity user = userRepository.findByEmail(email)
+        UserEntity user = IUserRepository.findByEmail(email)
                 .orElseThrow(
                         () -> new UsernameNotFoundException("User not found with this email:" + email)
                 );
