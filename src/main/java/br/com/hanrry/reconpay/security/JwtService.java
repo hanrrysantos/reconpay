c1e5553b7d77b383a2ea6c64aaacda6620b30f89
hanrrysantos <hanrry.jsantos@gmail.com>
2026-06-15 20:37:06 -0300

    feat: implementando classe JwtService, com metodos para gerar token, extrair email to token, validar o token e metodo para assinar o token

---
+++ src/main/java/br/com/hanrry/reconpay/security/JwtService.java
@@ -0,0 +1,59 @@
+package br.com.hanrry.reconpay.security;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.util.Date;
+
+@Component
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    private SecretKey getSignKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateToken(String email) {
+        Instant issuedAt = Instant.now();
+
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(Date.from(issuedAt))
+                .expiration(Date.from(issuedAt.plusMillis(expiration)))
+                .signWith(getSignKey())
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parser()
+                .verifyWith(getSignKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(getSignKey())
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        } catch (JwtException ex) {
+            return false;
+        }
+    }
+}
