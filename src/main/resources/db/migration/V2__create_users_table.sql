366b268eafdd5c1f22163d1b1ed0e09de33c5ff5
hanrrysantos <hanrry.jsantos@gmail.com>
2026-06-15 20:32:53 -0300

    feat: criando migration para criar tabela de users

---
+++ src/main/resources/db/migration/V2__create_users_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    name VARCHAR(150) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(20) NOT NULL DEFAULT 'USER' CHECK (role IN ('USER', 'ADMIN')),
+    active BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
