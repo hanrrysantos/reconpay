def65a74aeece70f825a8072863f6592e9c5e2e3
hanrrysantos <hanrry.jsantos@gmailcom>
2026-06-16 07:39:35 -0300

    feat: adicionando dois metodos, um para retornar todos os users ativos e outra para encontrar um user ativo

--- src/main/java/br/com/hanrry/reconpay/merchant/repository/IMerchantRepository.java
+++ src/main/java/br/com/hanrry/reconpay/merchant/repository/IMerchantRepository.java
@@ -3,6 +3,7 @@ package br.com.hanrry.reconpay.merchant.repository;
 import br.com.hanrry.reconpay.merchant.entity.MerchantEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -10,6 +11,8 @@ public interface IMerchantRepository extends JpaRepository<MerchantEntity, UUID>
 
     boolean existsByDocument(String document);
 
-    Optional<MerchantEntity> findByDocument(String document);
+    List<MerchantEntity> findAllByActiveTrue();
+
+    Optional<MerchantEntity> findByIdAndActiveTrue(UUID id);
 
 }
