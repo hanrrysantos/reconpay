c04e921f4c493c96b7856383000064d321da81d7
hanrrysantos <hanrry.jsantos@gmailcom>
2026-06-16 23:13:05 -0300

    feat: iniciando criacao da entidade FeeRules

---
+++ src/main/java/br/com/hanrry/reconpay/feeRules/entity/FeeRulesEntity.java
@@ -0,0 +1,32 @@
+package br.com.hanrry.reconpay.feeRules.entity;
+
+import br.com.hanrry.reconpay.feeRules.enums.PaymentMethod;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Table
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FeeRulesEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+
+    private UUID merchantId;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+
+
+
+}
