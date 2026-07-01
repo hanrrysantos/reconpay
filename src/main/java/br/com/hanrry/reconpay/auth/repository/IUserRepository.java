1f3e3985e1d0dabdc7a7c88b6b6e8d4d254826dd
hanrrysantos <hanrry.jsantos@gmailcom>
2026-07-01 15:00:41 -0300

    feat: ajustando logica para retornar apenas user com active = true

--- src/main/java/br/com/hanrry/reconpay/auth/repository/IUserRepository.java
+++ src/main/java/br/com/hanrry/reconpay/auth/repository/IUserRepository.java
@@ -3,6 +3,7 @@ package br.com.hanrry.reconpay.auth.repository;
 import br.com.hanrry.reconpay.auth.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -10,5 +11,9 @@ public interface IUserRepository extends JpaRepository<UserEntity, UUID> {
 
     boolean existsByEmail(String email);
 
-    Optional<UserEntity> findByEmail(String email);
+    List<UserEntity> findAllByActiveTrue();
+
+    Optional<UserEntity> findByIdAndActiveTrue(UUID id);
+
+    Optional<UserEntity> findByEmailAndActiveTrue(String email);
 }
