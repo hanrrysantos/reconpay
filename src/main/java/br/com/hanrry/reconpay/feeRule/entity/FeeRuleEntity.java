ec028fe533a40208cb04565cadb9fb823ffe2140
Hanrry <hanrry.jsantos@gmail.com>
2026-07-31 22:10:30 -0300

    refactor: move PaymentMethod enum to shared package
    Centralize payment method values for reuse across fee rules and transactions.

--- src/main/java/br/com/hanrry/reconpay/feeRule/entity/FeeRuleEntity.java
+++ src/main/java/br/com/hanrry/reconpay/feeRule/entity/FeeRuleEntity.java
@@ -1,7 +1,7 @@
 package br.com.hanrry.reconpay.feeRule.entity;
 
 import br.com.hanrry.reconpay.merchant.entity.MerchantEntity;
-import br.com.hanrry.reconpay.feeRule.enums.PaymentMethod;
+import br.com.hanrry.reconpay.shared.enums.PaymentMethod;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
