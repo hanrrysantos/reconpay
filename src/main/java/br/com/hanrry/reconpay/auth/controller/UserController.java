a1ea262295680fe9efabb1f911c4dae011bdf9f3
Hanrry <hanrry.jsantos@gmail.com>
2026-08-05 21:31:08 -0300

    fix: removed the URI

--- src/main/java/br/com/hanrry/reconpay/auth/controller/UserController.java
+++ src/main/java/br/com/hanrry/reconpay/auth/controller/UserController.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,9 +22,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.UUID;
 
 @RestController
@@ -39,11 +38,7 @@ public class UserController {
     public ResponseEntity<UserResponseDTO> createUser(
             @Valid @RequestBody CreateUserRequestDTO request) {
         UserResponseDTO user = userService.createUser(request);
-        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path("/api/users/{id}")
-                .buildAndExpand(user.id())
-                .toUri();
-        return ResponseEntity.created(uri).body(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 
     @GetMapping
