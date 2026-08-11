2f0711466053b0d1a78215f052573c703dd7d6a1
Hanrry <hanrry.jsantos@gmail.com>
2026-08-10 22:07:32 -0300

    feat: adding annotation @Override

--- src/main/java/br/com/hanrry/reconpay/auth/controller/AuthController.java
+++ src/main/java/br/com/hanrry/reconpay/auth/controller/AuthController.java
@@ -1,37 +1,30 @@
 package br.com.hanrry.reconpay.auth.controller;
 
 import br.com.hanrry.reconpay.auth.dto.AuthRequestDTO;
-import br.com.hanrry.reconpay.auth.dto.UserRequestDTO;
 import br.com.hanrry.reconpay.auth.dto.AuthResponseDTO;
+import br.com.hanrry.reconpay.auth.dto.UserRequestDTO;
 import br.com.hanrry.reconpay.auth.dto.UserResponseDTO;
+import br.com.hanrry.reconpay.auth.openapi.AuthControllerApi;
 import br.com.hanrry.reconpay.auth.service.AuthService;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@Tag(name = "Authentication")
-@RequestMapping("/api/auth")
-public class AuthController {
+public class AuthController implements AuthControllerApi {
 
     private final AuthService authService;
 
-    @PostMapping("/login")
-    public ResponseEntity<AuthResponseDTO> login(
-            @Valid @RequestBody AuthRequestDTO request){
+    @Override
+    public ResponseEntity<AuthResponseDTO> login(@Valid AuthRequestDTO request) {
         return ResponseEntity.ok(authService.login(request));
     }
 
-    @PostMapping("/register")
-    public ResponseEntity<UserResponseDTO> register(
-            @Valid @RequestBody UserRequestDTO request) {
+    @Override
+    public ResponseEntity<UserResponseDTO> register(@Valid UserRequestDTO request) {
         UserResponseDTO user = authService.register(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
