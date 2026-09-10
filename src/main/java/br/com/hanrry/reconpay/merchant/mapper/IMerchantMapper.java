edecaa298d0eac19af95f9414a470239cf325fa8
Hanrry <hanrry.jsantos@gmail.com>
2026-09-10 09:45:52 -0300

    fix: resolve mapstruct unmapped properties warning

--- src/main/java/br/com/hanrry/reconpay/merchant/mapper/IMerchantMapper.java
+++ src/main/java/br/com/hanrry/reconpay/merchant/mapper/IMerchantMapper.java
@@ -4,10 +4,16 @@ import br.com.hanrry.reconpay.merchant.dto.MerchantRequestDTO;
 import br.com.hanrry.reconpay.merchant.dto.MerchantResponseDTO;
 import br.com.hanrry.reconpay.merchant.entity.MerchantEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface IMerchantMapper {
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "active", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "feeRules", ignore = true)
     MerchantEntity toEntity(MerchantRequestDTO request);
 
     MerchantResponseDTO toDTO(MerchantEntity entity);
